ab54bc4840a7ce34bc5e51dcd7ec295ac8a9439b
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-05-16 15:15:56 +0200

    Small changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,24 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
     const button = (
         <>
             <div className="button_header">
                 <img src="../assets/bell.svg" />
-                <img src="../assets/profile.svg" />
+                <Link to="settings">
+                    <img src="../assets/profile.svg" />
+                </Link>
             </div>
         </>
     );
     return (
         <div className="logo">
             <div className="text_logo">
-                <img src="../assets/logo.png" />
+                <Link to="/">
+                    <img src="../assets/logo.png" />
+                </Link>
+
                 <p>M8map</p>
             </div>
             {props.show ? button : null}
