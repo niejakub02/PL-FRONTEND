c87efe2be51de9fd24bcdec53d1075f14ea2df72
niejakub02 <imaslanky@gmail.com>
2023-05-18 20:12:15 +0200

    PL-73 Dashboard inputs styling/logic tweaks

---
+++ src/utils/mockData.js
@@ -0,0 +1,9 @@
+export const autocompleteOptions = [
+    { type: 'CITY', value: 'Warsaw' },
+    { type: 'LANGUAGE', value: 'English' },
+    { type: 'LANGUAGE', value: 'Polish' },
+    { type: 'CITY', value: 'Lodz' },
+    { type: 'LANGUAGE', value: 'German' },
+    { type: 'CITY', value: 'Berlin' },
+    { type: 'CITY', value: 'New York' },
+]
