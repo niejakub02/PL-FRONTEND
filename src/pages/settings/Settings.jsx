5830d5d19194206d6cedd53dc71418d1507d886d
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-06-28 13:15:25 +0200

    Change button ready for inputs, textarea and Autocomplete

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -5,26 +5,84 @@ import "../../styles/Styles.css";
 import "../../pages/sign/Sign.css";
 import "./Settings.css";
 import Wrapper from "../../components/wrapper/Wrapper.jsx";
+import { useState } from "react";
 
 const Settings = ({ languages, handleOpen, user }) => {
+    const [disabled, setDisabled] = useState({
+        avatar: true,
+        name: true,
+        surname: true,
+        age: true,
+        languages: true,
+        description: true,
+    });
+
+    const changeDisabled = (value) => {
+        setDisabled((el) => ({
+            ...disabled,
+            [value]: !el[value],
+        }));
+    };
+
     return (
         <Wrapper handleOpen={handleOpen} type={"SETTINGS"}>
             <div className="overflow">
                 <BoxSettings
                     type={<AvatarBox name={user.name} img={user.avatar} />}
                     name={"AVATAR"}
+                    nameInput={"avatar"}
+                    change={changeDisabled}
                 />
                 <BoxSettings
-                    type={<input type="text" value={user.name} disabled />}
+                    type={
+                        <input
+                            type="text"
+                            defaultValue={user.name}
+                            disabled={disabled.name}
+                            className={
+                                disabled.name
+                                    ? "inputno_no_active"
+                                    : "inputno_active"
+                            }
+                        />
+                    }
                     name={"FIRST NAME"}
+                    nameInput={"name"}
+                    change={changeDisabled}
                 />
                 <BoxSettings
-                    type={<input type="text" value={user.surname} disabled />}
+                    type={
+                        <input
+                            type="text"
+                            defaultValue={user.surname}
+                            disabled={disabled.surname}
+                            className={
+                                disabled.surname
+                                    ? "inputno_no_active"
+                                    : "inputno_active"
+                            }
+                        />
+                    }
+                    nameInput={"surname"}
                     name={"LAST NAME"}
+                    change={changeDisabled}
                 />
                 <BoxSettings
-                    type={<input type="number" value={user.age} disabled />}
+                    type={
+                        <input
+                            type="number"
+                            defaultValue={user.age}
+                            disabled={disabled.age}
+                            className={
+                                disabled.age
+                                    ? "inputno_no_active"
+                                    : "inputno_active"
+                            }
+                        />
+                    }
                     name={"AGE"}
+                    nameInput={"age"}
+                    change={changeDisabled}
                 />
                 <BoxSettings
                     type={
@@ -37,38 +95,47 @@ const Settings = ({ languages, handleOpen, user }) => {
                             getOptionLabel={(option) => option.title}
                             defaultValue={[languages[0]]}
                             filterSelectedOptions
-                            readOnly
+                            readOnly={disabled.languages}
                             renderInput={(params) => (
                                 <TextField {...params} placeholder="SEARCH" />
                             )}
                         />
                     }
+                    nameInput={"languages"}
                     name={"LANGUAGES"}
+                    change={changeDisabled}
                 />
                 <BoxSettings
                     type={
                         <textarea
                             type="text"
-                            value={user.description}
-                            disabled
+                            defaultValue={user.description}
+                            disabled={disabled.description}
+                            className={
+                                disabled.description
+                                    ? "inputno_no_active"
+                                    : "inputno_active"
+                            }
                         />
                     }
+                    nameInput={"description"}
                     name={"DESCRIPTION"}
+                    change={changeDisabled}
                 />
             </div>
         </Wrapper>
     );
 };
 
-const BoxSettings = (props) => {
+const BoxSettings = ({ type, name, change, nameInput }) => {
     return (
         <div className="block_settings flexCC">
-            {props.type}
-            <button className="buttons">
+            {type}
+            <button className="buttons" onClick={() => change(nameInput)}>
                 <p>CHANGE</p>
                 <img src="../assets/pencil.png" />
             </button>
-            <p className="name_settings">{props.name}</p>
+            <p className="name_settings">{name}</p>
         </div>
     );
 };
